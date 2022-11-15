ea5899a85680c152b4b419ff6e79432dfe5d2463
Dave Amies <damies13@gmail.com>
2022-11-15 19:28:08 +1000

    Setup for publishing to pypi

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name="MyNoSQL",
+	version="0.0.1",
+	author="damies13",
+	author_email="damies13+mynosql@gmail.com",
+	description="MyNoSQL",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/damies13/mynosql",
+	packages=setuptools.find_packages(exclude=["build/*"]),
+	install_requires=['requests', 'HTTPServer'],
+	classifiers=[
+		"Development Status :: 3 - Alpha",
+		"Topic :: Database :: Database Engines/Servers",
+		"Programming Language :: Python :: 3.7",
+		"License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+	project_urls={
+		'Getting Help': 'https://github.com/damies13/mynosql',
+		'Source': 'https://github.com/damies13/mynosql',
+	},
+)
