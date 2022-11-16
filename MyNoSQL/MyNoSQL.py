a09bbabc3aaa17f18e19524d9f63b4a25f7f567a
Dave Amies <damies13@gmail.com>
2022-11-16 11:02:46 +1000

    Version 0.0.2.1668560565

--- MyNoSQL/MyNoSQL.py
+++ MyNoSQL/MyNoSQL.py
@@ -168,7 +168,7 @@ class MyNoSQLServer(BaseHTTPRequestHandler):
 
 
 class MyNoSQL:
-	version = "0.0.3"
+	version = "0.0.2.1668560565"
 	debuglvl = 7
 	timeout=600
 
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
 	name="MyNoSQL",
-	version="0.0.2.1668560482",
+	version="0.0.2.1668560565",
 	author="damies13",
 	author_email="damies13+mynosql@gmail.com",
 	description="MyNoSQL",
