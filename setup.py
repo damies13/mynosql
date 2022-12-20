da2c9afd0406e329a3fd30111c5de714c6d387ef
Dave Amies <damies13@gmail.com>
2022-12-21 09:11:10 +1000

    Version 0.0.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
 	name="MyNoSQL",
-	version="0.0.4",
+	version="0.0.5",
 	author="damies13",
 	author_email="damies13+mynosql@gmail.com",
 	description="MyNoSQL",
